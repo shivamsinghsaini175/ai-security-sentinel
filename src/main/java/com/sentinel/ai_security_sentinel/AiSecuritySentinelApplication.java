617538e0da26f9dda35fcd44b81c47c59f8ad452
shivamsinghsaini175 <shivamsinghsaini175@gmail.com>
2026-04-26 23:09:21 +0530

    initial setup

---
+++ src/main/java/com/sentinel/ai_security_sentinel/AiSecuritySentinelApplication.java
@@ -0,0 +1,13 @@
+package com.sentinel.ai_security_sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSecuritySentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSecuritySentinelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sentinel/ai_security_sentinel/AiSecuritySentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinel.ai_security_sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSecuritySentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
